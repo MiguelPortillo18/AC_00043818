c11eb6359d7354a5e386b780aa101be784bdb04d
MiguelPortillo18 <00043818@uca.edu.sv>
2021-04-17 23:51:44 -0600

    Add: Tarea labo 2

---
+++ Tarea laboratorio 2/tarea.asm
@@ -0,0 +1,30 @@
+        org     100h
+
+        section .text
+
+        mov     BX, 200h
+        
+        mov     AL, "M"
+        mov     CL, "A"
+        mov     DL, "M"
+        mov     DH, "P"
+
+        mov     [0200h], AL
+        mov     [0201h], CL
+        mov     [0202h], DL
+        mov     [0203h], DH
+
+;       Direccionamiento directo o absoluto
+        mov     AX, [200h]
+
+;       Direccionamiento indirecto por registro
+        mov     CL, [BX]
+
+;       Direccionamiento indirecto base mas indice
+        mov     DL, [BX + SI]
+
+;       Direccionamiento relativo por registro
+        mov     DH, [DI + 100h]
+
+
+        int     20h
